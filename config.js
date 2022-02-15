b924632d311fd898e8c741af9642d8103d2eb33a
vantai2000 <90109923+vantai2000@users.noreply.github.com>
2022-02-15 22:25:39 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Em à!',
+    introDesc: `Em quên mở Google Map hã?
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Thế này`,
+    btnIntro: 'hihi',
+    title: 'Anh đã chót yêu em từ lúc lần đầu gặp 😙',
+    desc: 'Nụ cười của em đã làm anh say, dám hỏi em thích anh chưa? ',
+    btnYes: 'Em thích Anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Vui quá à vậy cho anh hay tại sao em lại thích anh hihi <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/EdgarTai1601' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
